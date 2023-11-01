7c505602830baad195a1b047c7174d24640d2ea6
jangsumin <jsm0726@naver.com>
2023-11-02 01:07:12 +0900

    lodash 라이브러리를 사용해서 검색 창에 디바운싱 적용

---
+++ day3/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import _ from "lodash";
+
+const debounceHandler = _.debounce((event) => {
+  console.log(event.target.value);
+}, 500);
+</script>
+
+<template>
+  <div class="search">
+    <input @input="debounceHandler" type="text" placeholder="검색어 입력" />
+    <img
+      src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
+    />
+  </div>
+</template>
+
+<style scoped>
+.search {
+  position: relative;
+  width: 300px;
+}
+
+input {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 10px 12px;
+  border: 1px solid #bbb;
+  border-radius: 8px;
+  font-size: 14px;
+}
+
+img {
+  cursor: pointer;
+  position: absolute;
+  width: 17px;
+  top: 10px;
+  right: 12px;
+  margin: 0;
+}
+</style>
