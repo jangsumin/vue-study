5a91d72414717c195f15ccf4665f8c2900381c18
장수민 <jsm0726@naver.com>
2023-11-03 08:38:12 +0900

    하위 컴포넌트에서 font-family 스타일 속성 지정하지 않고도 적용

--- trip/src/components/Navbar.vue
+++ trip/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <header>TRIP IS WAITING</header>
+  <header>여행</header>
 </template>
 
 <style scoped>
@@ -12,12 +12,7 @@ header {
   padding: 0 20px;
   display: flex;
   align-items: center;
-  color: var(
-    --font-color
-  ); /* 투명도 때문에 폰트 컬러 적용 안되고, 폰트 패밀리 전역 지정 안되는 거 바꿔야 함 */
   background-color: var(--navbar-color);
-  opacity: 0.5;
-  font-family: "Pretendard";
   font-size: 20px;
   letter-spacing: 1px;
 }
